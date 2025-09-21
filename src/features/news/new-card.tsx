b08f748b76e90b9095b83fe8514d396f39016175
YnotnA <ynotna.nawak@gmail.com>
2025-09-21 12:52:50 +0200

    feat(news): update news card

--- src/features/news/new-card.tsx
+++ src/features/news/new-card.tsx
@@ -8,29 +8,47 @@ import {
 } from "@ui/card";
 import Link from "next/link";
 import type { News } from "./news-manager";
+import { Typography } from "@components/DS/typography";
+import { Badge } from "@ui/badge";
 
 type NewCardProps = {
   news: News;
 };
 
 export const NewCard = (props: NewCardProps) => {
+  const attributes = props.news.attributes;
   return (
     <Link href={`/news/${props.news.slug}`}>
       <Card className="transition-all hover:shadow-xl">
         <CardHeader className="h-fit">
-          <AspectRatio ratio={16 / 9} className="overflow-hidden rounded-md">
+          <div className="flex gap-4">
+            {attributes.titleIcon && (
+              <div className="text-5xl">{attributes.titleIcon}</div>
+            )}
+            <div className="flex flex-col justify-between gap-2">
+              <CardTitle>{attributes.title}</CardTitle>
+              <div className="flex gap-2">
+                {attributes.tags.map((tag) => (
+                  <Badge key={tag}>{tag}</Badge>
+                ))}
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <AspectRatio ratio={16 / 9} className="overflow-hidden">
             <img
-              src={props.news.attributes.coverUrl}
-              alt={props.news.attributes.title}
+              src={attributes.coverUrl}
+              alt={attributes.title}
               className="size-full object-cover"
             />
           </AspectRatio>
-        </CardHeader>
-        <CardContent className="space-y-2">
-          <CardTitle>{props.news.attributes.title}</CardTitle>
-          <CardDescription>{props.news.attributes.description}</CardDescription>
+          <CardDescription>
+            <Typography variant="p">{attributes.description}</Typography>
+          </CardDescription>
         </CardContent>
       </Card>
     </Link>
   );
 };
+
