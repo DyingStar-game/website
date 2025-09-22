4a205c80b9330b83af73e48f8f08faa0887df2e7
YnotnA <ynotna.nawak@gmail.com>
2025-09-22 08:54:24 +0200

    feat(news): modify default font

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ServerToaster } from "@feat/server-sonner/server-toaster";
 import { getServerUrl } from "@lib/server-url";
 import { cn } from "@lib/utils";
 import type { Metadata } from "next";
-import { Geist_Mono, Inter, Space_Grotesk } from "next/font/google";
+import { Poppins } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Suspense } from "react";
 import { SiteConfig } from "site-config";
@@ -18,26 +18,19 @@ export const metadata: Metadata = {
   metadataBase: new URL(getServerUrl()),
 };
 
-const CaptionFont = Space_Grotesk({
+const PoppinsFont = Poppins({
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
-  variable: "--font-caption",
-});
-
-const GeistSans = Inter({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
-
-const GeistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
 });
 
 export default function RootLayout({ children, modal }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className="from-background-1 via-background-2 to-background-3 text-foreground h-full bg-linear-120 bg-fixed"
+      className={cn(
+        PoppinsFont.className,
+        "from-background-1 via-background-2 to-background-3 text-foreground h-full bg-linear-120 bg-fixed",
+      )}
       suppressHydrationWarning
     >
       <body
@@ -46,9 +39,6 @@ export default function RootLayout({ children, modal }: LayoutProps<"/">) {
           "min-h-screen font-sans antialiased",
           "before:top-40 before:w-2/3 before:min-w-[1800px] before:bg-[url('/images/bg-circles.svg')]",
           "before:pointer-events-none before:fixed before:bottom-0 before:left-1/2 before:z-0 before:-translate-x-1/2 before:bg-cover before:bg-top before:bg-no-repeat before:opacity-30 before:content-['']",
-          GeistMono.variable,
-          GeistSans.variable,
-          CaptionFont.variable,
         )}
       >
         <NuqsAdapter>
