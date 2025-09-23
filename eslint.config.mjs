a10cc7489bb3b1f6269843ecb028aced56c60878
Hadrien Verrier <verrier.hadrien.pro@gmail.com>
2025-09-23 22:40:34 +0200

    fix(eslint): modifier le style de saut de ligne pour utiliser 'unix'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -66,7 +66,7 @@ export default [
           argsIgnorePattern: "^_|props|^_error",
         },
       ],
-      "linebreak-style": ["error", "auto"],
+      "linebreak-style": ["error", "unix"],
       semi: ["error", "always"],
       "no-extra-semi": "error",
       "default-case": 0,
