7907e62093957f76c834f4f0aecad49ab97c6264
Dercraker <antoine.capitain@gmail.com>
2025-10-04 22:08:29 +0200

    TEMP remove jiti on next build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
-
-import createJiti from "jiti";
 import createNextIntlPlugin from "next-intl/plugin";
-import { fileURLToPath } from "node:url";
-const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// import createJiti from "jiti";
+// import { fileURLToPath } from "node:url";
+
+// const jiti = createJiti(fileURLToPath(import.meta.url));
 const withNextIntl = createNextIntlPlugin();
 
-jiti("./src/lib/env/server.ts");
-jiti("./src/lib/env/client.ts");
-
+// jiti("./src/lib/env/server.ts");
+// jiti("./src/lib/env/client.ts");
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -26,7 +26,7 @@ const nextConfig: NextConfig = {
     ],
   },
   output: "standalone",
-  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  // transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
 };
 
 export default withNextIntl(nextConfig);
