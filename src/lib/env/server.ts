50343d21440dc0588709e0e12f765fe0bd0ce382
Dercraker <antoine.capitain@gmail.com>
2025-10-10 23:16:20 +0200

    update envs

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    NEXT_ENV: z.literal("production"),
     LAST_YOUTUBE_ID: z.string().optional(),
     DISCORD_INVITE_ID: z.string(),
     MEILI_URL: z.string(),
