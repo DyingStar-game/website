227cf64bd07696db1f04d2689bde9879c3d94d43
Dercraker <antoine.capitain@gmail.com>
2025-10-11 18:31:58 +0200

    update e nv

--- src/lib/env/client.ts
+++ src/lib/env/client.ts
@@ -5,14 +5,14 @@ import { version } from "../../../package.json";
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_SITE_URL: z.url(),
     NEXT_PUBLIC_APP_VERSION: z.string(),
+    NEXT_PUBLIC_SITE_URL: z.url(),
     NEXT_PUBLIC_DISCORD_INVITE_ID: z.string(),
     NEXT_PUBLIC_GITHUB_REPO: z.string(),
   },
   runtimeEnv: {
-    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
     NEXT_PUBLIC_APP_VERSION: version,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
     NEXT_PUBLIC_DISCORD_INVITE_ID: process.env.NEXT_PUBLIC_DISCORD_INVITE_ID,
     NEXT_PUBLIC_GITHUB_REPO: process.env.NEXT_PUBLIC_GITHUB_REPO,
   },
--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -3,11 +3,13 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    LAST_YOUTUBE_ID: z.string().optional(),
     MEILI_URL: z.string(),
     MEILI_MASTER_KEY: z.string(),
+
     GH_WEBHOOK_SECRET: z.string(),
     GITHUB_TOKEN: z.string(),
+
+    LAST_YOUTUBE_ID: z.string().optional(),
   },
   experimental__runtimeEnv: process.env,
 });
