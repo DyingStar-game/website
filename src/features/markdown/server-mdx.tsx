9b96fba680cd9aec0e9bb8a7919080aed276284a
YnotnA <ynotna.nawak@gmail.com>
2025-10-11 11:51:13 +0200

    feat(news): fix prose size and align img

--- src/features/markdown/server-mdx.tsx
+++ src/features/markdown/server-mdx.tsx
@@ -16,14 +16,15 @@ export const ServerMdx = (props: ServerMdxProps) => {
     <div
       className={cn(
         "prose max-w-full prose-dyingstar",
-        "prose-p:text-lg md:prose-p:text-xl lg:prose-p:text-2xl",
-        "prose-ul:text-lg md:prose-ul:text-xl lg:prose-ul:text-2xl",
-        "prose-ol:text-lg md:prose-ol:text-xl lg:prose-ol:text-2xl",
-        "prose-h1:text-5xl prose-h1:font-normal md:prose-h1:text-6xl lg:prose-h1:text-7xl",
-        "prose-h2:text-4xl prose-h2:font-normal md:prose-h2:text-5xl lg:prose-h2:text-6xl",
-        "prose-h3:text-xl prose-h3:font-normal md:prose-h3:text-2xl lg:prose-h3:text-3xl",
-        "prose-h4:text-xl prose-h4:font-normal",
+        // "prose-p:text-lg md:prose-p:text-xl lg:prose-p:text-2xl",
+        // "prose-ul:text-lg md:prose-ul:text-xl lg:prose-ul:text-2xl",
+        // "prose-ol:text-lg md:prose-ol:text-xl lg:prose-ol:text-2xl",
+        // "prose-h1:text-5xl prose-h1:font-normal md:prose-h1:text-6xl lg:prose-h1:text-7xl",
+        // "prose-h2:text-4xl prose-h2:font-normal md:prose-h2:text-5xl lg:prose-h2:text-6xl",
+        // "prose-h3:text-xl prose-h3:font-normal md:prose-h3:text-2xl lg:prose-h3:text-3xl",
+        // "prose-h4:text-xl prose-h4:font-normal",
         "prose-figure:flex prose-figure:flex-col prose-figure:items-center",
+        "prose-p:has-[img]:flex prose-p:has-[img]:flex-col prose-p:has-[img]:items-center",
         props.className,
       )}
     >
