98e44646fc3390e422bdb9e6a5d2233d5152eca7
Dercraker <antoine.capitain@gmail.com>
2025-10-12 11:24:19 +0200

    update sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -37,11 +37,12 @@ const generateNewsEntries = async (locale: Locale): Promise<SitemapEntry[]> => {
   );
 };
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
   const homepageEntries = generateHomePageEntries();
   const newsEntries = await Promise.all(
     LOCALES.map(async (locale) => generateNewsEntries(locale)),
   );
 
   return [...homepageEntries, ...newsEntries.flat()];
-}
+};
+export default sitemap;
