ef88625639e35acad384ec0fedaced29e47631ec
Dercraker <antoine.capitain@gmail.com>
2025-10-12 23:31:38 +0200

    Update static page

--- app/[locale]/(layout)/project/_components/projectDescriptionSection.tsx
+++ app/[locale]/(layout)/project/_components/projectDescriptionSection.tsx
@@ -1,18 +1,23 @@
 import { ServerMdx } from "@feat/markdown/serverMdx";
 import { LayoutSection } from "@feat/page/layout";
 import fs from "fs";
-import { getLocale } from "next-intl/server";
+import type { Locale } from "next-intl";
 import path from "path";
 
-export const ProjectDescriptionSection = async () => {
-  const locale = await getLocale();
+type ProjectDescriptionSectionProps = {
+  locale: Locale;
+};
 
+export const ProjectDescriptionSection = async ({
+  locale,
+}: ProjectDescriptionSectionProps) => {
   const markdownPath = path.join(
     process.cwd(),
-    `content/${locale}`,
+    `content`,
+    locale,
     `projectDescription.${locale}.md`,
   );
-  const markdown = fs.readFileSync(markdownPath, "utf-8");
+  const markdown = fs.readFileSync(markdownPath, "utf8");
 
   return (
     <LayoutSection size="container" className="gap-6">
