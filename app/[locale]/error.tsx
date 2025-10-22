5c2071f1c1e6d1933a686e16b644af97993f3d2d
YnotnA <ynotna.nawak@gmail.com>
2025-10-22 23:50:09 +0200

    fix(seo): fix error page

---
+++ app/[locale]/error.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Page400 } from "@feat/page/page-400";
+
+const ErrorPage = () => {
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <div className="flex flex-1 items-center justify-center">
+        <Page400 />
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
