6ddf181c9af35789cb45eefdf09d8daa1bf5a205
Dercraker <antoine.capitain@gmail.com>
2025-10-27 00:29:24 +0100

    fix(API): Fix webhook payload -> WIP graphql

--- app/api/github/webhook/route.ts
+++ app/api/github/webhook/route.ts
@@ -1,13 +1,10 @@
-import {
-  deleteProjectIssue,
-  updateProjectIssue,
-} from "@feat/api/github/hooks/indexedProjectIssues";
+import { updateProjectIssues } from "@feat/api/github/hooks/indexedProjectIssues";
 import { IssuesWebhookSchema } from "@feat/issue/get/issuesWebhook.model";
 import { env } from "@lib/env/server";
 import { logger } from "@lib/logger";
 import { route } from "@lib/zodRoute";
 import { NextResponse } from "next/server";
-import { z } from "zod";
+import z from "zod";
 
 export const POST = route //TODO: Create a webhook middleware to handle the webhook
   .query(
@@ -22,8 +19,54 @@ export const POST = route //TODO: Create a webhook middleware to handle the webh
 
     logger.info("Webhook received", body);
 
-    if (body.action === "deleted") await deleteProjectIssue(body.issue.node_id);
-    else await updateProjectIssue(body.issue.node_id);
+    try {
+      // if (body.action === "deleted")
+      //   await deleteProjectIssue(body.projects_v2_item.node_id);
+      // else await updateProjectIssue(body.projects_v2_item.node_id);
+    } catch (error) {
+      logger.error("Error processing webhook", error);
 
+      await updateProjectIssues();
+
+      return NextResponse.json(
+        { message: "Error processing webhook" },
+        { status: 500 },
+      );
+    }
     return NextResponse.json({ message: "Webhook received" }, { status: 200 });
   });
+
+// export async function POST(req: NextRequest) {
+//   // Parse query params
+//   const { searchParams } = new URL(req.url);
+//   const secret = searchParams.get("secret");
+
+//   if (secret !== env.GH_WEBHOOK_SECRET) {
+//     return NextResponse.json({ message: "Invalid Access" }, { status: 403 });
+//   }
+
+//   let body: unknown;
+//   try {
+//     body = await req.json();
+//   } catch {
+//     return NextResponse.json({ message: "Invalid JSON" }, { status: 400 });
+//   }
+//   logger.info("Webhook received", body);
+
+//   const parseResult = IssuesWebhookSchema.safeParse(body);
+//   if (!parseResult.success) {
+//     return NextResponse.json({ message: "Invalid body" }, { status: 400 });
+//   }
+
+//   const typedBody = parseResult.data;
+
+//   logger.info("Webhook received", typedBody);
+
+//   if (typedBody.action === "deleted") {
+//     await deleteProjectIssue(typedBody.projects_v2_item.node_id);
+//   } else {
+//     await updateProjectIssue(typedBody.projects_v2_item.node_id);
+//   }
+
+//   return NextResponse.json({ message: "Webhook received" }, { status: 200 });
+// }
--- src/features/issue/get/issuesWebhook.model.ts
+++ src/features/issue/get/issuesWebhook.model.ts
@@ -1,23 +1,56 @@
 import { z } from "zod";
 
-export const IssuesWebhookSchema = z.object({
+const issue_actions = [
+  // "assigned",
+  "closed",
+  "deleted",
+  "edited",
+  // "labeled",
+  "locked",
+  "opened",
+  "pinned",
+  "reopened",
+  "typed",
+  // "unassigned",
+  // "unlabeled",
+  "unlocked",
+  "unpinned",
+  "untyped",
+] as const;
+
+const projects_v2_item_actions = [
+  "created",
+  "deleted",
+  "edited",
+  "restored",
+] as const;
+
+const projects_v2_status_update_actions = [
+  "created",
+  "deleted",
+  "edited",
+] as const;
+
+const projectsV2BodySchema = z.object({
   action: z.enum([
-    "assigned",
-    "closed",
-    "deleted",
-    "edited",
-    "labeled",
-    "opened",
-    "reopened",
-    "transferred",
-    "unassigned",
-    "unlabeled",
-    "unlocked",
-    "unpinned",
+    ...projects_v2_item_actions,
+    ...projects_v2_status_update_actions,
   ]),
+  projects_v2_item: z.object({
+    node_id: z.string(),
+  }),
+});
+
+const issueBodySchema = z.object({
+  action: z.enum(issue_actions),
   issue: z.object({
     node_id: z.string(),
   }),
 });
+
+export const IssuesWebhookSchema = z.union([
+  projectsV2BodySchema,
+  issueBodySchema,
+]);
 
 export type IssuesWebhookType = z.infer<typeof IssuesWebhookSchema>;
