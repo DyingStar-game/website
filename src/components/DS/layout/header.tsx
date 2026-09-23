8be6b1429abc87795810292e42238d5d45d6308d
YnotnA <ynotna.nawak@gmail.com>
2026-09-23 14:28:20 +0200

    fix(header): center nav header

--- src/components/DS/layout/header.tsx
+++ src/components/DS/layout/header.tsx
@@ -4,14 +4,10 @@ import { useState } from "react";
 
 import { HeaderBase } from "@components/DS/layout/headerBase";
 import NavLink from "@components/DS/layout/navLink";
-import { LogoNameSvg } from "@components/svg/logoNameSvg";
-import { LocaleSwitcher } from "@feat/i18n/LocaleSwitcher";
 import { LINKS } from "@feat/navigation/Links";
-import { Link } from "@i18n/navigation";
 import type { buttonVariants } from "@ui/button";
 import type { VariantProps } from "class-variance-authority";
 import { useMotionValueEvent, useScroll } from "framer-motion";
-import { SiteConfig } from "siteConfig";
 
 export const Header = () => {
   const [size, setSize] =
@@ -24,30 +20,20 @@ export const Header = () => {
   });
 
   return (
-    <HeaderBase>
-      <NavLink link={LINKS.Project.Project} size={size} />
-      <NavLink link={LINKS.Project.Play} size={size} />
-
-      <Link
-        href={LINKS.Landing.Landing.href()}
-        className="order-first self-center transition-all hover:scale-95 xl:order-none"
-        aria-label={SiteConfig.title}
-      >
-        <LogoNameSvg className="h-10 w-auto xl:h-12" />
-      </Link>
-      <NavLink link={LINKS.Project.Contribute} size={size} />
-      <NavLink link={LINKS.News.All} size={size} />
-
-      {/* <Button
-        variant="ghost"
-        size={size}
-        onClick={() => alert("Todo wait for auth tasks")}
-        className="xl:hidden"
-      >
-        Auth
-      </Button> */}
-
-      <LocaleSwitcher className="xl:hidden" size={size} />
-    </HeaderBase>
+    <HeaderBase
+      start={
+        <>
+          <NavLink link={LINKS.Project.Project} size={size} />
+          <NavLink link={LINKS.Lore.History} size={size} />
+        </>
+      }
+      end={
+        <>
+          <NavLink link={LINKS.Project.Contribute} size={size} />
+          <NavLink link={LINKS.News.All} size={size} />
+          <NavLink link={LINKS.Project.Play} size={size} variant="outline" />
+        </>
+      }
+    />
   );
 };
